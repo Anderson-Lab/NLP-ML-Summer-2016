78e23928b23650c05c3518f3328068a9fe8f37ce
ctpowell <ctpowell3@gamil.com>
2016-06-22 19:52:34 -0400

    Update YelpPymongo.py

--- YelpPymongo.py
+++ YelpPymongo.py
@@ -11,6 +11,9 @@ client = MongoClient()
 #Setting context to new yelp database 
 db = client.yelp
 
+#if you want to search the review text change just replace husk with a key word or phrase. 
+#d = db.reviews.find({text: {"$regex" : ".*Husk.*"}})
+
 d = db.reviews.find({'business_id': 'LM71VvmoAWDD5z8h7XoVig'})
 counter = 0
 for n in d:
